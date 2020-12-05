cfa1dd19422f15cee9fc22b1bd7506a782ae8a93
yangchengming <yangchengming@jtlxz.com>
2020-12-05 21:49:23 +0800

    添加能力单位体

---
+++ Assets/EGamePlay/Combat/AbilityUnit/AbilityUnit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using ET;
+
+namespace EGamePlay.Combat.Ability
+{
+    /// <summary>
+    /// 能力单位体
+    /// </summary>
+    public class AbilityUnit : AbilityEntity
+    {
+        public object unitInitData { get; set; }
+
+
+        public override void Awake(object initData)
+        {
+            unitInitData = initData;
+        }
+    }
+}
