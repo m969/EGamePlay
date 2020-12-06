1e4d33f8b9c2ff1e449b6e5bea6712d7a9e47bdd
m969 <969041327@qq.com>
2020-12-07 01:22:20 +0800

    up

---
+++ Assets/Examples/RpgExample/Scripts/MaterialColorAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MaterialColorAnimation : MonoBehaviour
+{
+    public Color Color;
+    public float Duration;
+    public Ease Ease;
+    public UnityEngine.Events.UnityEvent OnComplete;
+
+    private void OnEnable()
+    {
+        var mat = GetComponent<Renderer>().material;
+        var color = mat.GetColor("_BaseColor");
+        DOTween.To(() => color, (x) => { mat.SetColor("_BaseColor", x); }, Color, Duration).SetEase(Ease).OnComplete(()=> { OnComplete?.Invoke(); });
+    }
+}
