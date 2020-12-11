8f2992f7150eac998d0a74b738ab14d3692d6cc8
yangchengming <yangchengming@jtlxz.com>
2020-12-11 22:03:08 +0800

    up

--- Assets/Code/UnityApp.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class UnityApp
-{
-    public static Game Game => new Game();
-    public static UnitySDK UnitySDK => new UnitySDK();
-}
